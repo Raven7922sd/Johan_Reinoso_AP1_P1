8cd88ff2e2b68978c7d4e5ea11cc6401ea3d8360
Ravensd7922 <johan_reinoso@ucne.edu.do>
2025-06-09 20:40:17 -0400

    Validación mejorada

--- Johan_Reinoso_AP1_P1/Components/Models/Aportes.cs
+++ Johan_Reinoso_AP1_P1/Components/Models/Aportes.cs
@@ -9,11 +9,11 @@ public class Aportes
 
     [Required(ErrorMessage = "El campo es obligatorio.")]
     [StringLength(100, ErrorMessage = "El campo no puede exceder los 100 caracteres.")]
+    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "El campo solo puede contener letras y espacios.")]
     public string Nombres { get; set; }=null!;
 
     [Required(ErrorMessage = "El campo es obligatorio.")]
-    [Range(0, double.MaxValue, ErrorMessage = "El aporte debe ser un número positivo de 0 hasta 1000000.")]
+    [Range(0.1, 1000000, ErrorMessage = "El aporte debe ser un número positivo de 0.1 hasta 1,000,000.00")]
     public double Monto { get; set; }
     public DateTime Fecha { get; set; } = DateTime.Now;
 }
-
